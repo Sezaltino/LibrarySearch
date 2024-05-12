570fb77533c5ab38b65ea9558d38d2c3096749b6
Gabriel Sezaltino <79762040+Sezaltino@users.noreply.github.com>
2024-05-11 23:21:37 -0300

    Second Commit

--- src/index.ts
+++ src/index.ts
@@ -7,34 +7,30 @@ function handleResponse(response: { items: any[] }) {
         const authors = Array.isArray(item.volumeInfo.authors) ? item.volumeInfo.authors.join(', ') : 'Unknown'; // Verifica se os autores são uma matriz e os une em uma string separada por vírgulas, caso contrário, define como 'Unknown'
         const newBook: Books = {
             Author: authors,
-            AverageRating: item.volumeInfo.averageRating || 0, // Se a média de avaliações não estiver presente, defina como 0
-            Categories: item.volumeInfo.categories || [], // Se as categorias não estiverem presentes, defina como uma matriz vazia
-            Description: item.volumeInfo.description || 'No description available', // Se a descrição não estiver presente, defina como 'No description available'
-            Links: item.volumeInfo.imageLinks || {}, // Se os links da imagem não estiverem presentes, defina como um objeto vazio
-            PageCount: item.volumeInfo.pageCount || 0, // Se o número de páginas não estiver presente, defina como 0
-            PublishedDate: item.volumeInfo.publishedDate || 'Unknown', // Se a data de publicação não estiver presente, defina como 'Unknown'
-            RatingCount: item.volumeInfo.ratingCount || 0, // Se o número de avaliações não estiver presente, defina como 0
-            Title: item.volumeInfo.title || 'Unknown' // Se o título não estiver presente, defina como 'Unknown'
+            AverageRating: item.volumeInfo.averageRating || 0,
+            Categories: item.volumeInfo.categories || [],
+            Description: item.volumeInfo.description || 'No description available',
+            Links: item.volumeInfo.imageLinks || {},
+            PageCount: item.volumeInfo.pageCount || 0,
+            PublishedDate: item.volumeInfo.publishedDate || 'Unknown',
+            RatingCount: item.volumeInfo.ratingCount || 0,
+            Title: item.volumeInfo.title || 'Unknown'
         };
 
         book.push(newBook);
         displayBookCards(book);
-        // Aqui você pode decidir o que deseja fazer com cada item, por exemplo, adicionar ao HTML
-        // document.getElementById("content")!.innerHTML += "<br>" + item;    
     }
 
 }
 
-// Função para criar um elemento <li> com os detalhes do livro
 function createBookCard(book: Books): HTMLDivElement {
     const div = document.createElement('div');
     div.classList.add('book-card');
 
-    // Usar a URL da imagem como background do card
     if (book.Links && book.Links.thumbnail) {
         div.style.backgroundImage = `url(${book.Links.thumbnail})`;
     } else {
-        div.style.backgroundImage = 'url(default-thumbnail.jpg)'; // Imagem padrão, se a URL da imagem estiver ausente
+        div.style.backgroundImage = 'url(default-thumbnail.jpg)';
     }
 
     const title = document.createElement('h2');
@@ -48,8 +44,6 @@ function createBookCard(book: Books): HTMLDivElement {
     const description = document.createElement('p');
     description.textContent = `Description: ${book.Description}`;
     div.appendChild(description);
-
-    // Adicione mais elementos conforme necessário para os outros detalhes do livro
 
     return div;
 }
